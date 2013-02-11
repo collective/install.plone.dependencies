6f1907cacedb6fdc16194b18262e351d04320ba0
sven <sven@so36.net>
2013-02-11 15:11:00 +0100

    Add: check for installed packages for fedora

--- plone.dependencies.fedora.sh
+++ plone.dependencies.fedora.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
-# Inspired by:  http://www.mirkopagliai.it/
 # Description:  will check and install Plone [http://plone.org] dependencies
 # License:      GPL
 # Version:      0.1.beta
 #================================================
 
-yum -y install yum-utils
+#yum -y install yum-utils
 
-yum -y groupinstall "Development Tools"
+#yum -y groupinstall "Development Tools"
 
-yum -y install wv xpdf libxslt-devel zlib-devel libxml2-devel python-devel python-lxml libgsasl-devel openssl openssl-devel glibc-devel ncurses-devel
+# if we do not use yum -y groupinstall "Development Tools"
+#yum -y install gcc gcc-c++ autoconf automake binutils gettext libtool make pkgconfig strace bison flex gdb patch
+
+
+#yum -y install wv xpdf libxslt-devel zlib-devel libxml2-devel python-devel python-lxml libgsasl-devel openssl openssl-devel glibc-devel ncurses-devel
+
+
+checkFEDORA()
+{
+FEDORARPM='gcc gcc-c++ autoconf automake binutils gettext libtool make pkgconfig strace bison flex gdb patch'
+    for package in $FEDORARPM; do
+    RPM_OK=$(rpm -qa $package)
+    if [ "" == "$RPM_OK" ]; then
+        echo "Installing $package"
+        # yum -y install $package
+    fi
+done
+}
+
+checkFEDORA
 
 # ToDo
-# - switch group install development tools, to only packages we need, otherwise we will install too much crap and it will be
-# harder to check all packages ...
-# - check for already installed packages
+# - do we want to check/install git ?
 # - yum -s according to the wiki [not tested, yet]
 # - rewrite header of this script, remove Inspired by, move that to credits
 # - some links: http://unix.stackexchange.com/questions/23085/yum-groupinstall-development-libraries
