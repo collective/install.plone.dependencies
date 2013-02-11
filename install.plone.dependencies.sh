0596b6816c0be0cbb4bdb1ff0522b85e1316d9c4
sven <sven@so36.net>
2013-02-11 14:11:35 +0100

    Add: squeeze to script

--- install.plone.dependencies.sh
+++ install.plone.dependencies.sh
@@ -65,28 +65,44 @@ debianINST()
 
 #debianINST
 #
-checkDEBIAN()
+checkPrecise()
 {
-    DEPS='build-essential libssl-dev libxml2-dev libxslt1-dev libbz2-dev zlib1g-dev python-setuptools python-dev libjpeg62-dev libreadline-gplv2-dev python-imaging wv poppler-utils'
-    for package in $DEPS; do
+    PRECISEDEPS='build-essential libssl-dev libxml2-dev libxslt1-dev libbz2-dev zlib1g-dev python-setuptools python-dev libjpeg62-dev libreadline-gplv2-dev python-imaging wv poppler-utils'
+    for package in $PRECISEDEPS; do
     PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $package | grep "install ok installed")
     #dpkg-query -l $package
     echo Checking for $package: $PKG_OK
     if [ "" == "$PKG_OK" ]; then
         echo "Installing $package"
-        #sudo apt-get --force-yes --yes install the.package.name
+        #apt-get --force-yes --yes install $package
     fi
 done
 
 
 }
 
+# We should support Squeeze only till Wheezy is released as 'stable'
+checkSqueeze()
+{
+    SQUEEZEDEBS='build-essential wv poppler-utils python2.6-dev python-imaging libssl-dev libjpeg62-dev zlib1g-dev libreadline5-dev libxml2-dev python-libxml2 libxslt1-dev python-libxslt1 libpcre3 libpcre3-dev xpdf-utils libreadline5 zlib1g  libjpeg62 libssl0.9.8 cron groff-base python-dev'
+    for package in $SQUEEZEDEBS; do
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $package | grep "install ok installed")
+    echo Checking for $package: $PKG_OK
+    if [ "" == "$PKG_OK" ]; then
+        echo "Installing $package"
+        #apt-get --force-yes --yes $package
+    fi
+done
+}
+
+
+
 # Here we will try to figure out which OS it is we are talking about
 # and assigning the right function to the right OS
 #=====================================================================
 
 
-checkDEBIAN
+#checkDEBIAN
 
 #todo:
 #function for fedora
