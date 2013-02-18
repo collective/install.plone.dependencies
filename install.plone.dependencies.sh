77aa25eedf8c9df2c542e619a382bf8b349bef88
sven <sven@so36.net>
2013-02-18 12:50:21 +0100

    Add: check for root user

--- install.plone.dependencies.sh
+++ install.plone.dependencies.sh
@@ -1,71 +1,22 @@
-#!/bin/bash
-# Inspired by:  http://www.mirkopagliai.it/
+#!/usr/bin/env bash
 # Description:  will check and install Plone [http://plone.org] dependencies
 # License:      GPL
 # Version:      0.1.b1
 #================================================
 
-# Colors
-#txtrst=$(tput sgr0) # Text reset
-#txtred=$(tput setaf 1) # Red
 
-#echo “Welcome to ${txtred} Checking for missing Dependencies .... ${txtrst}!”
+# Check if user is root or using sudo
+# Sanity Check: Test if the script runs as root
+if [ "$(whoami)" != root ] ; then
+    echo -n "\nPlease run this script as root!\n" >&amp;2
+    exit 1
+fi
+
 
 # First some functions ....
 #===========================
-# Debian/Ubuntu
-debianINST()
-{
-    # List of Dependencies to Install
-    DEPENDENCIES=(build-essential libssl-dev libxml2-dev libxslt1-dev libbz2-dev zlib1g-dev python-setuptools python-dev libjpeg62-dev libreadline-gplv2-dev python-imaging wv poppler-utils)
 
-    #apt-get runs in quiet mode to avoid lots of output
-    #echo -n "Updating System ..."
-    #apt-get -q -q update
-
-    # check them ....
-    PKGSTOINSTALL=""
-    for (( i=0; i<${tLen=${#DEPENDENCIES[@]}}; i++ )); do
-    #for (( i=0; i<${tLen=${#APT_FILES[@]}}; i++ )); do
-    #for i in "$APT_FILES"; do
-    #for i in "${APT_FILES[@]}"; do
-        if [[ ! `dpkg -l | grep -w "ii  ${DEPENDENCIES[$i]} "` ]]; then
-            PKGSTOINSTALL=$PKGSTOINSTALL" "${DEPENDENCIES[$i]}
-        else
-        PKGSTOINSTALL=$PKGSTOINSTALL" "${DEPENDENCIES[$i]}
-        fi
-
-    #echo -n "${packages}"
-    #dpkg -l "${packages}"
-    #PKG_OK=$(dpkg-query -W --showformat='${Status}\n' | grep -q "install ok installed")
-    #PKG_OK=$(dpkg-query -W --showformat='${Status}\n'${APT_FILES[$i]} | grep -q "install ok installed")
-    #if dpkg-query -Wf'${db:Status-abbrev}'2>/dev/null | grep -q '^i' ; then
-    #    printf 'Why yes, the package "%s" _is_ installed!\n' "$packages"
-    #else
-    #    printf 'I regret to inform you that the packages "%s" are not currently installed.\n' "$packages"
-    #fi
-    #$PKG_OK
-    #if [ "" = "$PKG_OK" ]; then
-    #    echo "No somelib. Setting up somelib."
-        #sudo apt-get --force-yes --yes install "${APT_FILES}"
-        #echo -n "Installing missing Dependencies "${APT_FILES}""
-    #else
-    #    echo -n "busted"
-
-    done
-    # If some dependencies are missing, install them
-    if [ "$PKGSTOINSTALL" != "" ]; then
-        echo -n "Installing missing Dependencies"
-        #apt-get --force-yes --yes install $PKGSTOINSTALL
-        echo -n "$PKGSTOINSTALL"
-    else
-        echo -n "busted Something went wrong"
-    fi
-}
-
-#debianINST
-#
-checkPrecise()
+installPrecise()
 {
     PRECISEDEPS='build-essential libssl-dev libxml2-dev libxslt1-dev libbz2-dev zlib1g-dev python-setuptools python-dev libjpeg62-dev libreadline-gplv2-dev python-imaging wv poppler-utils'
     for package in $PRECISEDEPS; do
@@ -77,12 +28,10 @@ checkPrecise()
         #apt-get --force-yes --yes install $package
     fi
 done
-
-
 }
 
 # We should support Squeeze only till Wheezy is released as 'stable'
-checkSqueeze()
+installSqueeze()
 {
     SQUEEZEDEBS='build-essential wv poppler-utils python2.6-dev python-imaging libssl-dev libjpeg62-dev zlib1g-dev libreadline5-dev libxml2-dev python-libxml2 libxslt1-dev python-libxslt1 libpcre3 libpcre3-dev xpdf-utils libreadline5 zlib1g  libjpeg62 libssl0.9.8 cron groff-base python-dev'
     for package in $SQUEEZEDEBS; do
