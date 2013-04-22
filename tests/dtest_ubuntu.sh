0a4fb31507cb48b35bd57e69723d0acd3050d966
sven <sven@so36.net>
2013-04-22 16:11:43 +0200

    Fix missing "-"

--- tests/dtest_ubuntu.sh
+++ tests/dtest_ubuntu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wget -no-check-certificate https://raw.github.com/collective/install.plone.dependencies/dev/check_dependencies.sh
-wget -no-check-certificate https://raw.github.com/collective/install.plone.dependencies/dev/helper_scripts/check_ubuntu.sh
+wget --no-check-certificate https://raw.github.com/collective/install.plone.dependencies/dev/check_dependencies.sh
+wget --no-check-certificate https://raw.github.com/collective/install.plone.dependencies/dev/helper_scripts/check_ubuntu.sh
