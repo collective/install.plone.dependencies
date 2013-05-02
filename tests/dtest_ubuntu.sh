f010f1f208d766611c0ad376d1e4db95349b1e9a
sven <sven@so36.net>
2013-05-02 12:18:54 +0200

    I am lazy and the script is dirty

--- tests/dtest_ubuntu.sh
+++ tests/dtest_ubuntu.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+# this script is really basic and without any checks, it is meant to be to tun on clean test boxex
+
+HELPDIR='helper_scripts'
+
+#download base script
 wget --no-check-certificate https://raw.github.com/collective/install.plone.dependencies/dev/check_dependencies.sh
+chmod +x check_dependencies.sh
+
+# prepare helper_scripts
+mkdir "$HELPDIR"
+cd "$HELPDIR"
 wget --no-check-certificate https://raw.github.com/collective/install.plone.dependencies/dev/helper_scripts/check_ubuntu.sh
+wget --no-check-certificate https://raw.github.com/collective/install.plone.dependencies/blob/dev/helper_scripts/check_debian.sh
+chmod +x check_ubuntu.sh
+chmod +x check_debian.sh
