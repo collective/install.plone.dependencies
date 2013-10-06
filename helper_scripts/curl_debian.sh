2f2e8ecce3b2e6ceedef3a2f46f09680b7c6a19a
sven <sven@so36.net>
2013-10-06 17:40:37 +0200

    Update

--- helper_scripts/curl_debian.sh
+++ helper_scripts/curl_debian.sh
@@ -1,4 +1,3 @@
 #curl -O https://raw.github.com/collective/install.plone.dependencies/master/helper_scripts/check_debian.sh |
 
-bash <(curl -s https://raw.github.com/collective/install.plone.dependencies/master/helper_scripts/check_debian.sh)
-
+bash -i <(wget -qO- https://raw.github.com/collective/install.plone.dependencies/curl/helper_scripts/debian_all.sh)
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -57,9 +57,9 @@ fi
 
 
 if [ "$OS" == 'wheezy' ]; then
-    . helper_scripts/check_debian.sh
+    . helper_scripts/curl_debian.sh
 elif [ "$OS" == 'jessie' ]; then
-    . helper_scripts/check_debian.sh
+    . helper_scripts/curl_debian.sh
 elif [ "$OS" == 'precise' ]; then
     . helper_scripts/check_precise.sh
 elif [ "$OS" == 'quantal' ]; then
