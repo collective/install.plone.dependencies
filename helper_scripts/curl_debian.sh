50fb83100a7f34436d8b2d87530df1533421ca1d
sven <sven@so36.net>
2013-10-06 17:00:26 +0200

    Curl test

---
+++ helper_scripts/curl_debian.sh
@@ -0,0 +1,4 @@
+#curl -O https://raw.github.com/collective/install.plone.dependencies/master/helper_scripts/check_debian.sh |
+
+bash <(curl -s https://raw.github.com/collective/install.plone.dependencies/master/helper_scripts/check_debian.sh)
+
