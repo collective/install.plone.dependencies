4c3e5324df66f29d40127ad01dc26e6f9428be56
sven <sven@so36.net>
2013-10-10 16:41:57 +0200

    Change path

--- helper_scripts/wget_debian.sh
+++ helper_scripts/wget_debian.sh
@@ -1,2 +1,2 @@
 #!/bin./bash
-bash -i <(wget -qO- https://raw.github.com/collective/install.plone.dependencies/curl/helper_scripts/debian.sh)
+bash -i <(wget -qO- https://raw.github.com/collective/install.plone.dependencies/wget/helper_scripts/debian.sh)
